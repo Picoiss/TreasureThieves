4ea564ec28ebf6e3edfc0291b580fe90777140a0
Max <msh254@uclive.ac.nz>
2024-05-06 21:15:27 +1200

    Made MainMenuController (WIP) SetupScreenController is mostly finished updated MAIN MENU fxml to include fxid and added interativity.

---
+++ src/main/java/seng201/team35/gui/MainMenuController.java
@@ -0,0 +1,23 @@
+package seng201.team35.gui;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.stage.Stage;
+import seng201.team35.services.CounterService;
+
+import java.awt.*;
+public class MainMenuController {
+    @FXML
+    private Button shopbuttonmainmenu;
+    @FXML
+    private Button inventorybuttonmainmenu;
+    @FXML
+    private Button nextroundbuttonmainmenu;
+    @FXML
+    private Button modifier1button;
+    @FXML
+    private Button modifier2button;
+
+}
--- src/main/java/seng201/team35/gui/SetupScreenController.java
+++ src/main/java/seng201/team35/gui/SetupScreenController.java
@@ -2,43 +2,49 @@ package seng201.team35.gui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.stage.Stage;
 import seng201.team35.services.CounterService;
 
+import java.awt.*;
+
 /**
  * Controller for the gameSetup.fxml window
- * @author nsr36
+ * @author nsr36, msh254
  */
 public class SetupScreenController {
     @FXML
-    private Label defaultLabel;
-
+    private TextField playerNameTextField;
     @FXML
-    private Button defaultButton;
-
-    private CounterService counterService;
-
+    private Button continueButton;
+    @FXML
+    private Slider numRoundsSlider;
+    @FXML
+    private ComboBox difficultyComboBox;
+    private String playerName;
+    private int numRounds;
+    private String gameDifficulty;
     /**
      * Initialize the window
-     *
-     * @param stage Top level container for this window
-     */
-    public void init(Stage stage) {
-        counterService = new CounterService();
-    }
-
-    /**
-     * Method to call when our counter button is clicked
-     *
      */
     @FXML
-    public void nameChanged() {
+    public void getName() {
+        playerName = playerNameTextField.getText();
+    }
+    public void getRounds() {
+        numRounds = (int) numRoundsSlider.getValue();
 
     }
-
+    public void getDifficulty() {
+        gameDifficulty = (String) difficultyComboBox.getValue();
+    }
     @FXML
     public void gameSetupComplete() {
-
+        getName();
+        getRounds();
+        getDifficulty();
+        //is this where we switch screens? IN FUTURE add a call to the next window (Main Menu)
     }
 }

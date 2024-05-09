54234d843c1ecbe0f7484a003224a22a9f24c036
Max <msh254@uclive.ac.nz>
2024-05-09 12:39:55 +1200

    Added Inventory Controller Editied Inventory FXML file to include textAreas which can then be altered.

---
+++ src/main/java/seng201/team35/gui/InventoryController.java
@@ -0,0 +1,61 @@
+package seng201.team35.gui;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.stage.Stage;
+import seng201.team35.services.CounterService;
+import javafx.scene.control.TextArea;
+public class InventoryController {
+    @FXML
+    private TextArea towername1;
+    @FXML
+    private TextArea towername2;
+    @FXML
+    private TextArea towername3;
+    @FXML
+    private TextArea towername4;
+    @FXML
+    private TextArea towername5;
+    @FXML
+    private TextArea towespeed1;
+    @FXML
+    private TextArea towespeed2;
+    @FXML
+    private TextArea towespeed3;
+    @FXML
+    private TextArea towespeed4;
+    @FXML
+    private TextArea towespeed5;
+    @FXML
+    private TextArea towerrecourseamount1;
+    @FXML
+    private TextArea towerrecourseamount2;
+    @FXML
+    private TextArea towerrecourseamount3;
+    @FXML
+    private TextArea towerrecourseamount4;
+    @FXML
+    private TextArea towerrecourseamount5;
+    @FXML
+    private TextArea towerlevel1;
+    @FXML
+    private TextArea towerlevel2;
+    @FXML
+    private TextArea towerlevel3;
+    @FXML
+    private TextArea towerlevel4;
+    @FXML
+    private TextArea towerlevel5;
+    @FXML
+    private TextArea towercost1;
+    @FXML
+    private TextArea towercost2;
+    @FXML
+    private TextArea towercost3;
+    @FXML
+    private TextArea towercost4;
+    @FXML
+    private TextArea towercost5;
+}

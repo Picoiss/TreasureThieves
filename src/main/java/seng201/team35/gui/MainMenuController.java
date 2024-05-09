1ccb789b34b66c1534e952d2f573cd15742bcb73
Max <msh254@uclive.ac.nz>
2024-05-09 12:50:25 +1200

    Main Menu Controller Updated.

--- src/main/java/seng201/team35/gui/MainMenuController.java
+++ src/main/java/seng201/team35/gui/MainMenuController.java
@@ -5,7 +5,6 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.stage.Stage;
-import seng201.team35.GameManager;
 import seng201.team35.services.CounterService;
 
 import java.awt.*;
@@ -20,7 +19,28 @@ public class MainMenuController {
     private Button modifier1button;
     @FXML
     private Button modifier2button;
-    private GameManager gameManager;
-    public MainMenuController(GameManager x) { gameManager = x; }
-
+    @FXML
+    private Button modifier3button;
+    private Boolean hasmodifierbeenselected = false;
+    @FXML
+    public void shopclicked() {
+        // transiiton to shop screen
+    }
+    @FXML
+    public void inventoryclicked() {
+        //transition to inventory screen
+    }
+    @FXML
+    public void nextroundclicked() {
+        if (hasmodifierbeenselected = true) {
+            // transition to next round
+        }
+        else {
+            // implelement a warning to select a Modifier
+        }
+    }
+    @FXML
+    public void modifier1clicked() {
+        hasmodifierbeenselected = true;
+    }
 }

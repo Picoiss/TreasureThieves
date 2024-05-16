f31d7ef5d58a96aae3d98b58baeebb7a9f1265c2
Max <msh254@uclive.ac.nz>
2024-05-16 23:16:42 +1200

    For some reason Path and SpriteSheet didnt get commited.

---
+++ src/main/java/seng201/team35/models/SpriteSheet.java
@@ -0,0 +1,65 @@
+package seng201.team35.models;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SpriteSheet {
+    private Map<String, Image> spriteMap;
+    private static final String DEFAULT_SPRITE = "/images/Ruby Dragon.png";
+    private static final String SPRITE_PATH = "/images/";
+
+    private static final int FRAME_COUNT = 5;
+
+    public SpriteSheet() {
+        spriteMap = new HashMap<>();
+        initializeSprites();
+    }
+
+    private void initializeSprites() {
+        String[] towers = {
+                "Bronze Archer", "Bronze Dwarf", "Bronze Villager",
+                "Silver Knight", "Silver Priest", "Silver Assassin",
+                "Gold Giant", "Gold Goblin", "Gold Wolf",
+                "Diamond Mage", "Diamond Necromancer", "Diamond Minotaur",
+                "Emerald Elf", "Emerald Pheonix", "Emerald Pegasus",
+                "Ruby Dragon", "Ruby Wyvern", "Ruby Golem"
+        };
+
+        for (String tower : towers) {
+            String imagePath = SPRITE_PATH + tower + ".png";
+            Image image;
+            try {
+                image = new Image(getClass().getResourceAsStream(imagePath));
+            } catch (Exception e) {
+                image = new Image(getClass().getResourceAsStream(DEFAULT_SPRITE)); // i defaulted the sprite to Ruby Dragon
+                // was the first sprite i had edited..
+            }
+            spriteMap.put(tower, image);
+            //make sure the grid we use does not contain a tower. (popualte the arraylist? no map thing idk what its called)
+        }//hashmap..
+    }
+
+    public Image getSprite(String towerName) {
+        return spriteMap.getOrDefault(towerName, spriteMap.get("Ruby Dragon"));
+        //return the sprite for the towername, or the sporite for ruby dragon.
+    }
+
+    public Image getSpriteFrame(String towerName, int frameIndex) { // god this was awful to code.
+        Image spriteSheet = getSprite(towerName); // set the image to the sprite image..
+        int spriteSheetWidth = (int) spriteSheet.getWidth(); // width is the width of the image
+        int spriteSheetHeight = (int) spriteSheet.getHeight(); // height is the height of the image.. yes.
+        int frameWidth = spriteSheetWidth / FRAME_COUNT; // frame count is here. not (5) because i was trying things.
+        // most likely a redundant class variable.
+        int frameHeight = spriteSheetHeight;
+        int x = frameIndex * frameWidth;
+        int y = 0;
+        return new WritableImage(spriteSheet.getPixelReader(), x, y, frameWidth, frameHeight); // yet to understand.
+    }
+
+    public int getFrameCount() {
+        return FRAME_COUNT; // redundant function.. (dont remove though as there will be dependencies)
+    }
+}

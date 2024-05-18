cb5f392e95bca9ca93f53facd01e11d814b79748
nsr36 <nsr36@uclive.ac.nz>
2024-05-18 13:09:47 +1200

    Edited names of Main Menu variables and FXML controls to match Java naming conventions according to the project specification

--- src/main/java/seng201/team35/gui/MainMenuController.java
+++ src/main/java/seng201/team35/gui/MainMenuController.java
@@ -1,14 +1,13 @@
 package seng201.team35.gui;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
-import javafx.scene.effect.Glow;
-import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import seng201.team35.GameManager;
-import seng201.team35.models.Tower;
-import java.util.ArrayList;
+
 import java.util.Arrays;
-import java.util.Random;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Controller for the Main Menu.fxml window
@@ -16,23 +15,11 @@ import java.util.List;
  */
 public class MainMenuController {
     @FXML
-    private Button shopbuttonmainmenu;
+    private TextArea modifierText1;
     @FXML
-    private Button inventorybuttonmainmenu;
+    private TextArea modifierText2;
     @FXML
-    private Button nextroundbuttonmainmenu;
-    @FXML
-    private Button modifier1button;
-    @FXML
-    private TextArea modifiertext1; // make a label
-    @FXML
-    private TextArea modifiertext2;
-    @FXML
-    private TextArea modifiertext3;
-    @FXML
-    private Button modifier2button;
-    @FXML
-    private Button modifier3button;
+    private TextArea modifierText3;
     @FXML
     private Label noModifierWarning;
     @FXML
@@ -45,12 +32,12 @@ public class MainMenuController {
     private Label roundsLabel;
     @FXML
     private Label roundsLeftLabel;
-    private final List<String> modifiers = Arrays.asList(new String[]{"Tower Speed Increase 10%", "Tower Speed Increase 5%", "Tower Speed Decrease 5%",
-            "Tower Speed Decrease 10%", "Cart Number Decrease by 2", "Cart Number Decrease by 1", "Cart Number Increase by 1", "Cart Number Increase by 2", "Cart Fill Amount Decrease 10%"
-    , "Cart Fill Amount Increase 5%", "Cart Fill Amount Increase 10%", "Cart Fill Amount Decrease 5%"});
-
-    private Boolean hasmodifierbeenselected = false;
-    private String modifiername;
+    private final String[] MODIFIERS = {"Tower Speed Increase 10%", "Tower Speed Increase 5%", "Tower Speed Decrease 5%", "Tower Speed Decrease 10%",
+            "Cart Number Decrease by 2", "Cart Number Decrease by 1", "Cart Number Increase by 1", "Cart Number Increase by 2", "Cart Fill Amount Decrease 10%",
+            "Cart Fill Amount Increase 5%", "Cart Fill Amount Increase 10%", "Cart Fill Amount Decrease 5%"};
+    private final List<String> MODIFIERSLIST = Arrays.asList(MODIFIERS);
+    private Boolean modifierSelected = false;
+    private String modifierName;
     private GameManager gameManager;
     public MainMenuController(GameManager x) { gameManager = x; }
     public void initialize() {
@@ -58,9 +45,9 @@ public class MainMenuController {
         int randomModifier1 = rng.nextInt(0,4);
         int randomModifier2 = rng.nextInt(4,8);
         int randomModifier3 = rng.nextInt(8,12);
-        modifiertext1.setText(modifiers.get(randomModifier1));
-        modifiertext2.setText(modifiers.get(randomModifier2));
-        modifiertext3.setText(modifiers.get(randomModifier3));
+        modifierText1.setText(MODIFIERSLIST.get(randomModifier1));
+        modifierText2.setText(MODIFIERSLIST.get(randomModifier2));
+        modifierText3.setText(MODIFIERSLIST.get(randomModifier3));
         playerNameLabel.setText(gameManager.getPlayerName());
         livesLabel.setText(String.valueOf(gameManager.getLives()));
         moneyLabel.setText(String.valueOf(gameManager.getMoneyAmount()));
@@ -68,18 +55,18 @@ public class MainMenuController {
         roundsLeftLabel.setText(String.valueOf(gameManager.getNumOfRounds() - gameManager.getCurrentRound()));
     }
     @FXML
-    public void shopclicked() {
+    public void shopClicked() {
         gameManager.mainMenuToShopScreen();
     }
     @FXML
-    public void inventoryclicked() {
+    public void inventoryClicked() {
         gameManager.mainMenuToInventoryScreen();
     }
     @FXML
-    public void nextroundclicked() {
+    public void nextRoundClicked() {
         gameManager.mainMenuToGameScreen();
         System.out.println("NextRoundClicked");
-        if (hasmodifierbeenselected == true) {
+        if (modifierSelected) {
             if (gameManager.getMainTowerList().size() < 5) {
                 noModifierWarning.setText("Please have at least 5 main towers");
             }
@@ -90,30 +77,30 @@ public class MainMenuController {
         }
         else {
             System.out.println("ModifierNotSelected");
-            System.out.println(modifiers);
+            System.out.println(MODIFIERSLIST);
             noModifierWarning.setText("Please select a Modifier");
         }
     }
     @FXML
-    public void modifier1clicked() {
-        hasmodifierbeenselected = true;
-        modifiername = modifiertext1.getText();
-        System.out.println(modifiername);
+    public void modifier1Clicked() {
+        modifierSelected = true;
+        modifierName = modifierText1.getText();
+        System.out.println(modifierName);
         System.out.println("Modifier 1 has been selected");
 
     }
     @FXML
-    public void modifier2clicked() {
-        hasmodifierbeenselected = true;
-        modifiername = modifiertext2.getText();
-        System.out.println(modifiername);
+    public void modifier2Clicked() {
+        modifierSelected = true;
+        modifierName = modifierText2.getText();
+        System.out.println(modifierName);
         System.out.println("Modifier 2 has been selected");
     }
     @FXML
-    public void modifier3clicked() {
-        hasmodifierbeenselected = true;
-        modifiername = modifiertext3.getText();
-        System.out.println(modifiername);
+    public void modifier3Clicked() {
+        modifierSelected = true;
+        modifierName = modifierText3.getText();
+        System.out.println(modifierName);
         System.out.println("Modifier 3 has been selected");
     }
 }
